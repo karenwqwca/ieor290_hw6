602fc0599cfc5aa21c740eaaa0e549aec4f96f8b
Qiwen Wu <karenwu@karenwu-macbookpro.roam.corp.google.com>
2017-03-25 23:35:16 -0700

    final version

--- hw6_problem3.py
+++ hw6_problem3.py
@@ -2,7 +2,7 @@ from scipy import linalg
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-from sklearn.svm import SVC
+from sklearn import svm
 from sklearn import discriminant_analysis
 from sklearn import metrics
 
@@ -30,39 +30,35 @@ plt.ylim(yy.min(), yy.max())
 ax = plt.gca()
 z = logit.predict_proba(np.c_[xx.ravel(),yy.ravel()])[:,1]
 z = z.reshape(xx.shape)
-cs = ax.contourf(xx,yy,z,cmap='RdBu',alpha=.5)
-cs2 = ax.contour(xx,yy,z,cmap='RdBu',alpha=.5)
-plt.clabel(cs2,fmt= '%2.1f', colors = 'k', fontsize=14)
-
+plt.contour(xx, yy, z, [0.5], linewidths=2., colors='k')
 plt.scatter(x[:,0],x[:,1],c=y,edgecolors='k',cmap=plt.cm.Paired)
+plt.axis('tight')
+plt.title('Logistic Regression Analysis')
 plt.show()
 
 logit_labels_train = logit.predict(x)
 print ("logit train accuracy:",format(metrics.accuracy_score(y,logit_labels_train)))
-# ('logit train accuracy:', '0.936666666667')
 
-# fisher's linear discriminant
-lda = discriminant_analysis.LinearDiscriminantAnalysis(store_covariance=True,n_components=2)
-lda.fit(x,np.ravel(y))
-lda_labels_train = lda.predict(x)
-zz_lda = logit.predict_proba(np.c_[xx.ravel(),yy.ravel()])[:,1]
+# fisher's linear discriminant & quadratic discriminant analysis
+lda = discriminant_analysis.LinearDiscriminantAnalysis(solver="svd", store_covariance=True)
+lda_labels_train = lda.fit(x,np.ravel(y)).predict(x)
+zz_lda = lda.predict_proba(np.c_[xx.ravel(),yy.ravel()])[:,1]
 zz_lda = zz_lda.reshape(xx.shape)
 
-plt.figure()
+
 splot = plt.subplot(1, 2, 1)
-plt.scatter(x[:,0],x[:,1], c=y,edgecolors='k',cmap=plt.cm.Paired)
+plt.scatter(x[:,0],x[:,1], c=y,edgecolors='k')
 plt.contour(xx, yy, zz_lda, [0.5], linewidths=2., colors='k')
 plt.legend()
 plt.axis('tight')
 plt.title('Linear Discriminant Analysis')
 
-qda = discriminant_analysis.QuadraticDiscriminantAnalysis()
-qda.fit(x,y)
-qda_labels_train = qda.predict(x)
-zz_qda = logit.predict_proba(np.c_[xx.ravel(),yy.ravel()])[:,1]
+qda = discriminant_analysis.QuadraticDiscriminantAnalysis(store_covariances=True)
+qda_labels_train = qda.fit(x,y).predict(x)
+zz_qda = qda.predict_proba(np.c_[xx.ravel(),yy.ravel()])[:,1]
 zz_qda = zz_qda.reshape(xx.shape)
 splot = plt.subplot(1, 2, 2)
-plt.scatter(x[:,0],x[:,1], c=y,edgecolors='k',cmap=plt.cm.Paired)
+plt.scatter(x[:,0],x[:,1], c=y,edgecolors='k')
 plt.contour(xx, yy, zz_qda, [0.5], linewidths=2., colors='k')
 plt.legend()
 plt.axis('tight')
@@ -71,5 +67,31 @@ plt.show()
 
 print ("lda train accuracy:",format(metrics.accuracy_score(y,lda_labels_train)))
 print ("qda train accuracy:",format(metrics.accuracy_score(y,qda_labels_train)))
-# ('lda train accuracy:', '0.943333333333')
-# ('qda train accuracy:', '0.953333333333')
+
+# svm
+svm_linear_kernel = svm.SVC(kernel='linear', C=1).fit(x,y)
+svm_labels_train = svm_linear_kernel.predict(x)
+zz_svm_linear_kernel = svm_linear_kernel.predict(np.c_[xx.ravel(),yy.ravel()])
+zz_svm_linear_kernel = zz_svm_linear_kernel.reshape(xx.shape)
+
+splot = plt.subplot(1, 2, 1)
+plt.scatter(x[:,0],x[:,1], c=y,edgecolors='k')
+plt.contour(xx, yy, zz_svm_linear_kernel, [0.5], linewidths=2., colors='k')
+plt.legend()
+plt.axis('tight')
+plt.title('SVC with linear kernel')
+
+svm_rbf = svm.SVC(kernel='rbf', C=1).fit(x,y)
+rbf_svc_labels_train = svm_rbf.predict(x)
+zz_svm_rbf = svm_rbf.predict(np.c_[xx.ravel(),yy.ravel()])
+zz_svm_rbf = zz_svm_rbf.reshape(xx.shape)
+splot = plt.subplot(1, 2, 2)
+plt.scatter(x[:,0],x[:,1], c=y,edgecolors='k')
+plt.contour(xx, yy, zz_svm_rbf, [0.5], linewidths=2., colors='k')
+plt.legend()
+plt.axis('tight')
+plt.title('SVC with RBF kernel')
+plt.show()
+
+print ("svm linear kernel train accuracy:",format(metrics.accuracy_score(y,svm_labels_train)))
+print ("svm rbf train accuracy:",format(metrics.accuracy_score(y,rbf_svc_labels_train)))
